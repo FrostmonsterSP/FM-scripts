260b88d9c7378c29589f0cd4894af2ea7a2e9953
Frost Monster <frostmonster@yandex.ru>
2022-05-16 21:56:38 +0300

    update-zsh-settings.sh is almost done

--- update-zsh-settings.sh
+++ update-zsh-settings.sh
@@ -4,18 +4,26 @@
 LOGSTATUS=false
 ECHOSTATUS=true
 FORCESTATUS=false
+REMOTEUPDATE=true
+CREATELINKS=true
+TESTMSG=false
 
 # Custom settings container
 for m in "$@"; do
-    if [[ "$m" == "-"* ]]; then SETTINGS+=("$m"); fi
+    if [[ "$m" == "-"* ]]; then SETTINGS+=("${m//-/}"); fi
 done
 
 # Setting custom script's settings
 for param in "${SETTINGS[@]}"; do
-    # Case doesn't work, I don't know why. Using if's
-    if [[ "$param" == *"l"* ]] && [ -f "./logging.sh" ]; then LOGSTATUS=true; elif [[ "$param" == *"l"* ]] && ! [ -f "./logging.sh" ]; then smsg "Parameter \"l\" is set, but file \".\\logging.sh\" is not found. Logs cannot be written." "-e"; fi
-    if [[ "$param" == *"e"* ]]; then ECHOSTATUS=false; fi
-    if [[ "$param" == *"f"* ]]; then FORCESTATUS=true; ECHOSTATUS=false; fi
+    if [[ "$param" == *"l"* ]] && [ -f "./logging.sh" ]; then LOGSTATUS=true; elif [[ "$param" == *"l"* ]] && ! [ -f "./logging.sh" ]; then smsg "Parameter \"l\" is set, but file \"logging.sh\" is not found. Logs cannot be written." "-e"; fi
+    case $param in 
+        *"e"*) ECHOSTATUS=false;;
+        *"f"*) FORCESTATUS=true; ECHOSTATUS=false;;
+        *"r"*) REMOTEUPDATE=false;;
+        *"x"*) CREATELINKS=false;;
+        *"t"*) TESTMSG=true
+    esac
+    if [[ "$param" == "e" ]]; then ECHOSTATUS=false; fi
 done
 
 # Connecting "./logging.sh" if logging turned on
@@ -29,7 +37,7 @@ smsg () {
         # Case doesn't work, I don't know why. Using if's
         if [[ $2 == *"e"* ]]; then MSGTYPE="\e[1;31mERROR:\e[0m "; LOGPARAM="-e"; fi
         if [[ $2 == *"c"* ]]; then MSGTYPE="\e[1;31mCRITICAL:\e[0m "; LOGPARAM="-c"; fi
-        if [[ $2 == *"n"* ]]; then MSGTYPE="";LOGPARAM="-n"; fi
+        if [[ $2 == *"n"* ]]; then MSGTYPE=""; LOGPARAM="-n"; fi
     elif [[ "$2" ]]; then MSGTYPE="\e[1;34m$2:\e[0m "; LOGPARAM="$2"
     else MSGTYPE=""; LOGPARAM="";
     fi
@@ -43,31 +51,52 @@ update () {
         then smsg "Checking and downloading updates"; cd /usr/share/zsh/core || return; git fetch; git pull --recurse-submodules> /dev/null 2>&1; smsg "Settings files downloaded"
         else smsg "zsh/core/ dirrectory haven't found" "-e"; smsg "Cloning settings from repository"; git clone --recursive https://github.com/FrostmonsterSP/FMZshConfig.git /usr/share/zsh/core > /dev/null 2>&1; smsg "Updates checked and downloaded"
     fi
+    chmod +x -R /usr/share/zsh/core
+}
+
+# Adds linck to core/.zshrc to users folders. Input is user folder
+userlinks () {
+    if [ -f /usr/share/zsh/core/.zshrc ]
+        then
+            smsg "Creating link in $1, user ${1//*\//}"
+            ln -sf "/usr/share/zsh/core/.zshrc" "$1/.zshrc"; #chown -h "${1//*\//}":"${1//*\//}" "$1/.zshrc"
+            if [ "$(find $1 -maxdepth 1 -xtype l | grep .zshrc)" ]; then smsg "Created link is broken, try run script with \"-l\" parameter to fix it" "-e"; fi
+        else
+            smsg "No congiguration file in zsh/core/" "-c"; exit 1
+    fi
 }
 
 # Checking .zshrc file in users folders
 checkfiles () {
-    if [ -f "/root/.zshrc" ];
-            then smsg "Файлы есть"
-            else smsg "Файлов нет"
+    if [ -L "/root/.zshrc" ];
+            then smsg "Config file link in /root exists"
+            else smsg "Config file link in /root doesn't exists"; userlinks "/root"
         fi
     userdirs=(/home/*)
     for userdir in "${userdirs[@]}"; do
-        if [ -f "$userdir/.zshrc" ];
-            then smsg "Файлы есть"
-            else smsg "Файлов нет"
+        if [ -L "$userdir/.zshrc" ];
+            then smsg "Config file link in $userdir exists"
+            else smsg "Config file link in $userdir doesn't exists"; userlinks "$userdir"
         fi
     done
 }
 
-# Adds linck to core/.zshrc to users folders
-userlinks () { true; }
+sctest () {
+    smsg "Regular message"
+    smsg "Custom type message" "Custom type"
+    smsg "Line break message" "-n"
+    smsg "Error message" "-e"
+    smsg "Critical error message" "-c"
+    exit 0
+}
 
-#chmod +x -R /usr/share/zsh/core
+if $TESTMSG; then sctest; fi
 
 # Root privileges checking
 if [[ $EUID -ne 0 ]]; then smsg "Root privilegies requeried. Run this script under \"sudo\" or root user" "-c"; fi
 
-update
+if $REMOTEUPDATE; then update; fi
+if $CREATELINKS; then checkfiles; fi
+if false; then schelp; fi
 
 smsg "ZSH Settings updated" "-n"
